af3e80ab07ab458bd5c72ff343c01b5709eaa38c
FRANCOIS Julien <julien.francois@hetic.net>
2023-02-02 12:32:40 +0100

    docker ok

--- app/config/init.sql
+++ app/config/init.sql
@@ -10,93 +10,75 @@ values
     ('Hello World', 'The obligatory Hello World Post ...'),
     ('Another Post', 'Yet another blog post about something exciting');
 
--- CREATE TABLE IF NOT EXISTS posts (
---     id int not null unique,
---     title varchar(64),
---     content text,
---     primary key(id)
--- );
---
--- CREATE TABLE IF NOT EXISTS user (
---     idUser int NOT NULL AUTO_INCREMENT,
---     email varchar(64) NULL ,
---     name varchar(64) NULL ,
---     password varchar(64) NULL ,
---     role int NULL ,
---     PRIMARY KEY (
---         idUser
---     )
--- );
---
--- CREATE TABLE IF NOT EXISTS professional (
---     idProfessional int NOT NULL AUTO_INCREMENT ,
---     idUser int  NOT NULL ,
---     idTimeSlot int  NOT NULL ,
---     name varchar(64)  NULL ,
---     adress varchar(64)  NULL ,
---     service varchar(64)  NULL ,
---     PRIMARY KEY (
---         idProfessional
---     )
--- );
---
--- CREATE TABLE IF NOT EXISTS customer (
---     idCustomer int NOT NULL AUTO_INCREMENT ,
---     idUser int  NOT NULL ,
---     PRIMARY KEY (
---         idCustomer
---     )
--- );
---
--- CREATE TABLE IF NOT EXISTS employee (
---     idEmployee int  NOT NULL ,
---     name varchar(64)  NOT NULL ,
---     description varchar(128)  NOT NULL ,
---     price int  NOT NULL ,
---     PRIMARY KEY (
---         idEmployee
---     )
--- );
---
--- CREATE TABLE IF NOT EXISTS slot (
---     idSlot int  NOT NULL AUTO_INCREMENT ,
---     idTimeSlot int  NOT NULL ,
---     idCustomer int  NOT NULL ,
---     idEmployee int  NOT NULL ,
---     day date NULL ,
---     hour int NULL ,
---     PRIMARY KEY (
---         idSlot
---     )
--- );
---
--- CREATE TABLE IF NOT EXISTS timeSlot (
---     idTimeSlot int  NOT NULL AUTO_INCREMENT ,
---     day date  NOT NULL ,
---     opening int  NOT NULL ,
---     closure int  NOT NULL ,
---     PRIMARY KEY (
---         idTimeSlot
---     )
--- );
---
--- ALTER TABLE user ADD CONSTRAINT `fk_user_idUser` FOREIGN KEY(idUser)
---     REFERENCES customer (idUser);
---
--- ALTER TABLE professional ADD CONSTRAINT `fk_professional_idUser` FOREIGN KEY(idUser)
---     REFERENCES user (idUser);
---
--- ALTER TABLE professional ADD CONSTRAINT `fk_professional_idTimeSlot` FOREIGN KEY(idTimeSlot)
---     REFERENCES timeSlot (idTimeSlot);
---
--- ALTER TABLE slot ADD CONSTRAINT `fk_slot_idCustomer` FOREIGN KEY(idCustomer)
---     REFERENCES customer (idCustomer);
---
--- ALTER TABLE slot ADD CONSTRAINT `fk_slot_idEmployee` FOREIGN KEY(idEmployee)
---     REFERENCES employee (idEmployee);
---
--- ALTER TABLE timeSlot ADD CONSTRAINT `fk_timeSlot_idTimeSlot` FOREIGN KEY(idTimeSlot)
---     REFERENCES slot (idTimeSlot);
+create table user (
+    idUser serial not null unique,
+    email varchar(64) null,
+    name varchar(64) null,
+    password varchar(64) null,
+    role INTEGER NULL,
+    primary key(idUser)
+);
+
+insert into user(email, name, password, role)
+values
+    ('juan@gmail.com', 'juan', 'password', 0),
+    ('alex@gmail.com', 'alex', 'password', 1);
+
+CREATE TABLE IF NOT EXISTS professional (
+    idProfessional SERIAL PRIMARY KEY ,
+    idUser INTEGER  ,
+    idTimeSlot INTEGER  NOT NULL ,
+    name VARCHAR(64)  NULL ,
+    adress VARCHAR(64)  NULL ,
+    service VARCHAR(64)  NULL ,
+
+    CONSTRAINT fk_user  FOREIGN KEY(idUser)
+        REFERENCES user(idUser) ON DELETE CASCADE,
+
+    CONSTRAINT fk_timeSlot FOREIGN KEY(idTimeSlot)
+        REFERENCES timeSlot(idTimeSlot)  ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS customer (
+    idCustomer SERIAL PRIMARY KEY ,
+    idUser INTEGER ,
+    CONSTRAINT fk_idUser FOREIGN KEY(idUser)
+        REFERENCES user (idUser) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS employee (
+    idEmployee SERIAL PRIMARY KEY ,
+    name VARCHAR(64)  NOT NULL,
+    description VARCHAR(128)  NOT NULL,
+    price INTEGER   NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS slot(
+    idSlot SERIAL PRIMARY KEY ,
+    idTimeSlot INTEGER  NOT NULL ,
+    idCustomer INTEGER  NOT NULL ,
+    idEmployee INTEGER  NOT NULL ,
+    day DATE NULL ,
+    hour INTEGER  NULL ,
+
+    CONSTRAINT fk_idCustomer FOREIGN KEY(idCustomer)
+        REFERENCES customer (idCustomer) ON DELETE CASCADE,
+
+    CONSTRAINT fk_idEmployee FOREIGN KEY(idEmployee)
+        REFERENCES employee (idEmployee) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS timeSlot (
+    idTimeSlot SERIAL PRIMARY KEY,
+    day DATE NOT NULL ,
+    opening INTEGER  NOT NULL ,
+    closure INTEGER  NOT NULL ,
+    idSlot INTEGER ,
+    CONSTRAINT fk_idSlot FOREIGN KEY(idSlot)
+        REFERENCES slot(idTimeSlot) ON DELETE CASCADE
+);
+
+
 --
 -- INSERT INTO user(email, name, password, role) VALUES ('juan@gmail.com', 'Juan', 'juanpassword', 0), ('alex@gmail.com', 'Alex', 'alexpassword', 1), ('julien@gmail.com', 'Julien', 'julienpassword', 1);
 --
