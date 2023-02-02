cbfaeac0befa79d46785c98357655e6e1a976c2d
FRANCOIS Julien <julien.francois@hetic.net>
2023-02-02 12:00:15 +0100

    reformat docker

--- app/controller/server.go
+++ app/controller/server.go
@@ -27,6 +27,6 @@ func Start() {
 	router = mux.NewRouter()
 
 	initHandlers()
-	fmt.Printf("router initialized and listening on 3400\n")
-	log.Fatal(http.ListenAndServe(":3400", router))
+	fmt.Printf("router initialized and listening on 3200\n")
+	log.Fatal(http.ListenAndServe(":3200", router))
 }
