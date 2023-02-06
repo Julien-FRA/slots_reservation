7e730722dd7c2b55d7c273be8d16d6073db71c4a
Juan Restrepo Torres <juan.m13@hotmail.fr>
2023-02-07 00:23:07 +0100

    added CreateShopForm + homepage image

--- app/views/src/pages/dashboard-nested-pages/Shop.tsx
+++ app/views/src/pages/dashboard-nested-pages/Shop.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import CreateShop from "../../components/Inc/CreateShop";
 
 const Shop = () => (
     <div>
         <h1>This is Shop SubPage</h1>
+        <CreateShop/>
     </div>
 );
 
