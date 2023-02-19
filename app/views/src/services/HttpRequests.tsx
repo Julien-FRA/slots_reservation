a684cbec965f33dce28a8f1774477beb5660353a
Juan Restrepo Torres <juan.m13@hotmail.fr>
2023-02-19 18:15:55 +0100

    small fix

--- app/views/src/services/HttpRequests.tsx
+++ app/views/src/services/HttpRequests.tsx
@@ -20,9 +20,9 @@ export const GetAllUsers = async (): Promise<User | false> => (
     .then((response) => {
       return response.data;
     })
-    .catch((error) => false);
-
-export const GetEmployeesWorkingHours = async (): Promise<User | false> =>
+    .catch((error) => false)
+)
+export const GetEmployeesWorkingHours = async (): Promise<User | false> => (
   await axios
     .get("http://localhost:3200/api/working-hours-shop/1")
     .then((response) => {
